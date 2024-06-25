4d13bf11beccb3b7837bf3139e210f40ba5e2ee0
Rahul <rahulgupta12786@gmail.com>
2024-06-25 06:35:21 +0530

    adding backend conf and yaml pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,12 @@ terraform {
     }
   }
 
-  # backend "azurerm" {
-  #       resource_group_name   = "myResourceGroup"
-  #       storage_account_name  = "mystorageaccount"
-  #       container_name        = "tfstate"
-  #       key                   = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+        resource_group_name   = "myResourceGroup"
+        storage_account_name  = "mystorageaccount"
+        container_name        = "tfstate"
+        key                   = "terraform.tfstate"
+  }
 
 }
 
