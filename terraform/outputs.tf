8272f0c02ac182957bd7a9b72018ccf990449408
Rahul <rahulgupta12786@gmail.com>
2024-06-25 06:11:54 +0530

    updating gitignore and final outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,16 +1,3 @@
-output "nic_id" {
-  value = module.virtual-machine.nic_id
-}
-
-output "subnet_id" {
-  value = module.network.subnet_id
-  
-}
-
-output "nsg_id" {
-  value = module.network.nsg_id   
-}
-
 output "lb_pub_ip" {
   value = module.lb.lbpubip
   
